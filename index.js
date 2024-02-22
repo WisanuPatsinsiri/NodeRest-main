5cc694d8ba9d929de8761c203c983cdb83c180f3
WisanuPatsinsiri <bestzazazazath@gmail.com>
2024-02-23 00:09:10 +0700

    s

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express()
 
 
 app.use(express.json())
-
-const dbUrl = 'postgres://webadmin:YLMino65033@node59039-bestt.proen.app.ruk-com.cloud:11901/Books'
+const dbUrl = 'postgres://webadmin:YLMino65033@node59039-bestt.proen.app.ruk-com.cloud/Books'
 
 const sequelize = new Sequelize(dbUrl);
 
