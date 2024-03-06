5480f81942c041dcba9eeae6c941349e5fafa87e
Best <bestzazazazath@gmail.com>
2024-03-06 09:44:15 +0700

    save

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Sequelize = require('sequelize')
 const app = express()
-const cors = require(cors)
+const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
